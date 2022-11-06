9e01d353b51c362c9a03f6ada9fb20d227d4e3b3
alessandrovaru <alessandrovaru@gmail.com>
2022-11-06 16:26:25 -0400

    Adding GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
       "Bienvenido al Porfolio de UNICONORTE. Productora Audiovisual establecida en Caracas, Venezuela.",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-YN0YYXD2C6",
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-sass",
