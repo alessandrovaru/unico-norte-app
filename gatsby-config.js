05e2e3151159fbc7a1366adc696a1dad8407328d
alessandrovaru <alessandrovaru@gmail.com>
2022-11-06 20:48:37 -0400

    Changes to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,12 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-YN0YYXD2C6",
+          'G-YN0YYXD2C6',
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: true,
+          head: false,
         },
       },
     },
