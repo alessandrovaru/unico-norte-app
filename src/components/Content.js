b3705da30e902b2a81ceb1b23168fa88efc3ba7e
Alessandro <alessandrovaru@gmail.com>
2023-11-06 09:35:50 -0400

    Add wrapper div around iframes to control aspect ratio

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,17 +6,44 @@ export const HTMLContent = ({ content, className }) => {
   React.useEffect(() => {
     // Find any iframes in the content
     const iframes = document.querySelectorAll(`.${className} iframe`);
+  
+    // Create a wrapper div around each iframe to control aspect ratio
     iframes.forEach(iframe => {
-      // Set the width and height on the iframe
+      // Create a wrapper element for the iframe
+      const wrapper = document.createElement('div');
+      // Set the CSS class for styling
+      wrapper.className = 'iframe-wrapper';
+      // Use padding-top to create the aspect ratio effect
+      wrapper.style.position = 'relative';
+      wrapper.style.paddingTop = '56.25%'; // 16:9 aspect ratio
+      // Ensure the iframe is absolutely positioned within the wrapper
+      iframe.style.position = 'absolute';
       iframe.style.width = '100%';
-      iframe.style.height = '500px';
+      iframe.style.height = '100%';
+      iframe.style.left = '0';
+      iframe.style.top = '0';
+  
+      // Insert the wrapper into the DOM and move the iframe inside it
+      iframe.parentNode.insertBefore(wrapper, iframe);
+      wrapper.appendChild(iframe);
     });
-
-    // Cleanup function to reset styles on unmount
+  
+    // Cleanup function to remove the wrapper and reset styles on unmount
     return () => {
       iframes.forEach(iframe => {
+        // Remove the iframe from the wrapper and remove the wrapper
+        const wrapper = iframe.parentNode;
+        if(wrapper.className === 'iframe-wrapper') {
+          wrapper.parentNode.insertBefore(iframe, wrapper);
+          wrapper.parentNode.removeChild(wrapper);
+        }
+  
+        // Reset iframe styles
+        iframe.style.position = '';
         iframe.style.width = '';
         iframe.style.height = '';
+        iframe.style.left = '';
+        iframe.style.top = '';
       });
     };
   }, [content, className]); // Depend on content and className to re-apply styles if they change
