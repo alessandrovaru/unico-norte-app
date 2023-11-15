5fb1c5f185b5bfcdfb431df81c91544740d00eed
Alessandro <alessandrovaru@gmail.com>
2023-11-15 19:20:08 -0400

    Update Navbar link to Spanish

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/portfolio">
-                Porfolio
+                Nuestro trabajo
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contacto
