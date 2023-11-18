84343247e0e9bd9e8ce612fb903cbd53f3e3c2f9
Alessandro <alessandrovaru@gmail.com>
2023-11-18 19:02:11 -0400

    Refactor BlogRollTemplate component

--- src/components/BlogRoll/BlogRoll.js
+++ src/components/BlogRoll/BlogRoll.js
@@ -4,6 +4,7 @@ import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from '../PreviewCompatibleImage'
 import './styles.sass'
 import VanillaTilt from 'vanilla-tilt';
+import Posts from './Posts'
 
 function Tilt(props) {
   const { options, ...rest } = props;
@@ -21,50 +22,43 @@ function Tilt(props) {
 class BlogRollTemplate extends React.Component {
   render() {
     const { data } = this.props
-    // Filtrar los posts para incluir solo los que tienen featuredpost como true
-    const featuredPosts = data.allMarkdownRemark.edges.filter(({ node: post }) => post.frontmatter.featuredpost === true);
+    // Filtrar solo los posts con featuredpost igual a true
+    const featuredPosts = data.allMarkdownRemark.edges.filter(
+      ({ node: post }) => post.frontmatter.featuredpost === true
+    );
+    const allPosts = data.allMarkdownRemark.edges;
+
     const options = {
       speed: 1000,
       max: 5
     };
+
     return (
       <div className="columns is-multiline">
-        {featuredPosts.map(({ node: post }) => (
-          <Tilt options={options} className="blog-post is-parent column is-6" key={post.id}>
-            <article className={`blog-list-item tile is-child box notification is-featured`}>
-              <header>
-                {post.frontmatter.featuredimage ? (
-                  <div className="featured-thumbnail">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: post.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                      }}
-                    />
-                  </div>
-                ) : null}
-                <p className="post-meta">
-                  <Link
-                    className="title has-text-primary is-size-4"
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <br />
-                  <span className="subtitle is-size-5 is-block">
-                    {post.frontmatter.subtitle}
-                  </span>
-                </p>
-              </header>
-            </article>
-          </Tilt>
-        ))}
+        {window.location.pathname === '/portfolio' ? (
+          <Posts 
+            posts={allPosts} 
+            Tilt={Tilt} 
+            options={options} 
+            PreviewCompatibleImage={PreviewCompatibleImage} 
+            Link={Link} 
+          />  
+        ) : (
+          <Posts 
+            posts={featuredPosts} 
+            Tilt={Tilt} 
+            options={options} 
+            PreviewCompatibleImage={PreviewCompatibleImage} 
+            Link={Link} 
+          />  
+        )
+        }
       </div>
     )
   }
 }
 
-BlogRoll.propTypes = {
+BlogRollTemplate.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array,
@@ -79,7 +73,9 @@ export default function BlogRoll() {
         query BlogRollQuery {
           allMarkdownRemark(
             sort: { order: DESC, fields: [frontmatter___date] }
-            filter: { frontmatter: { templateKey: { eq: "blog-post" }, featuredpost: { eq: true } } }
+            filter: {
+              frontmatter: { templateKey: { eq: "blog-post" }}
+            }
           ) {
             edges {
               node {
---
+++ src/components/BlogRoll/Posts.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Posts = (props) => {
+  const {posts, Tilt, options, PreviewCompatibleImage, Link } = props;
+
+  
+  return (
+    <>
+      {posts.map(({ node: post }) => (
+        <Tilt options={options} className="blog-post is-parent column is-6" key={post.id}>
+          <article
+            className={`blog-list-item tile is-child box notification ${
+              post.frontmatter.featuredpost ? 'is-featured' : ''
+            }`}
+          >
+            <header>
+              {post.frontmatter.featuredimage ? (
+                <div className="featured-thumbnail">
+                  <PreviewCompatibleImage
+                    imageInfo={{
+                      image: post.frontmatter.featuredimage,
+                      alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                    }}
+                  />
+                </div>
+              ) : null}
+              <p className="post-meta">
+                <Link
+                  className="title has-text-primary is-size-4"
+                  to={post.fields.slug}
+                >
+                  {post.frontmatter.title}
+                </Link>
+                <br />
+                <span className="subtitle is-size-5 is-block">
+                  {post.frontmatter.subtitle}
+                </span>
+              </p>
+            </header>
+          </article>
+        </Tilt>
+      ))}
+    </>
+  )
+}
+
+export default Posts
